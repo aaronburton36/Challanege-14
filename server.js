09918085ed5330d1dd125d5d6cb487a3e27b8f05
aaron burton <aaronburton36@gmail.com>
2023-05-01 06:56:12 -0400

    fixing file structure

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./controllers/api/config/connection');
+const sequelize = require('./Server/controllers/api/config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -25,13 +25,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const helpers = require('./controllers/utils/helpers');
+const helpers = require('./Server/controllers/utils/helpers');
 const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-const routes = require('./controllers');
+const routes = require('./Server/controllers');
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
